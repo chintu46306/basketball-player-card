cc4a59a1e898ab8d5d076c7b3acf1fe07f46f570
chintu46306 <crg46306@gmail.com>
2024-07-12 10:33:38 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BasketballPlayerCard from './BasketballPlayerCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Basketball Player Card</h1>
+      <BasketballPlayerCard
+        name="LeBron James"
+        position="Forward"
+        stats={{ pointsPerGame: 25.4, assistsPerGame: 7.1, reboundsPerGame: 10.5 }}
+      />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './App.css';
+import './image.png';
+import './BasketballPlayerCard.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
